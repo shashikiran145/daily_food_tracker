2ad8e21eeb0ed0758f3a9fcc76580f3be797f809
Shashi Kiran Shastry <sshashikiransharma@gmail.com>
2026-04-26 20:54:03 +0200

    Update firebase.ts

--- foodtracker/lib/firebase.ts
+++ foodtracker/lib/firebase.ts
@@ -1,30 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// // import { initializeApp } from "firebase/app";
-// // import { getAnalytics } from "firebase/analytics";
-
-// import { initializeApp, getApps, getApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAdyvzPBvmSL0xuC--fF3MkBj_ZDDtf4EA",
-//   authDomain: "dailyfoodtracker-84b86.firebaseapp.com",
-//   projectId: "dailyfoodtracker-84b86",
-//   storageBucket: "dailyfoodtracker-84b86.firebasestorage.app",
-//   messagingSenderId: "1051463307762",
-//   appId: "1:1051463307762:web:74bdb590570f3dca2e5b6b",
-//   measurementId: "G-WS9NZZTBMK"
-// };
-
-// // Initialize Firebase
-// export const app =
-//   getApps().length === 0
-//     ? initializeApp(firebaseConfig)
-//     : getApp();
-
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
